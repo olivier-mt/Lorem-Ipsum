85584c201f06f72d2cf2492e1ace8469b1581211
Olivier Mountou <oliviermountou@MacBook-Pro-de-Olivier.local>
2021-04-26 16:19:10 +0200

    Component Content fixed

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,15 @@
+import Description from "./Description";
+import Title from "./Title";
+import "./App.css";
+
 const Content = (props) => {
-  return <div className={`${props.style}`}></div>;
+  return (
+    <div className={props.styleToApply}>
+      <Title name={props.title}></Title>
+
+      <Description text={props.text}></Description>
+    </div>
+  );
 };
 
 export default Content;
